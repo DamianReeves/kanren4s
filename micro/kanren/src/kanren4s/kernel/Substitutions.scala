c7ca3930eb952e3a9d1a2baf340a36861a3fb287
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-05-21 15:21:51 -0400

    Use extend

--- micro/kanren/src/kanren4s/kernel/Substitutions.scala
+++ micro/kanren/src/kanren4s/kernel/Substitutions.scala
@@ -87,12 +87,10 @@ object Substitutions {
       (t1, t2) match {
         case (t1 @ Variable(_, _), t2) =>
           // Try and extend the substitution since we have a variable on the left
-          if (s.occursCheck(t1, t2)) None
-          else Some(s.assign(t1, t2))
+          s.extend(t1, t2)
         case (t1, t2 @ Variable(_, _)) =>
           // Try and unify by flipping since we have a variable on the right
-          if (s.occursCheck(t2, t1)) None
-          else Some(s.assign(t2, t1))
+          s.extend(t2, t1)
         case (Term.Pair(l1, r1), Term.Pair(l2, r2)) =>
           // Unify the left and right sides of the pair
           unify(l1, l2)(s).flatMap(s1 => unify(r1, r2)(s1))
